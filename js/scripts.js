86f10ae929c7207b3c49757de9fdb054232f4727
Maciek Kaczmarczyk <maciejkaczmarczyk95@gmail.com>
2016-08-03 23:39:05 +0200

    skills section done

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$('.skill').hover(function(){
+  var prBar = $(this).children('.progressbar');
+
+  prBar.fadeIn(300)
+
+  prBar.children('.percent-20').addClass('percent-20-animation');
+  prBar.children('.percent-40').addClass('percent-40-animation');
+  prBar.children('.percent-60').addClass('percent-60-animation');
+  prBar.children('.percent-80').addClass('percent-80-animation');
+  prBar.children('.percent-100').addClass('percent-100-animation');
+});
+
+$('.env').hover(function(){
+  $(this).children('.logo-80x80')
+    .addClass('env-logo-animation')
+    .delay(1000)
+    .queue(function () {
+      $(this).removeClass('env-logo-animation')
+        .dequeue();
+    })
+
+});
